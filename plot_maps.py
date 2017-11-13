b081da31f6d1e4cfb9164410794c64d24d5704fd
Jonathan Griffin <jono.d.griffin@gmail.com>
2017-11-13 20:10:29 +1100

    Cleanering plotting with arrows

--- plot_maps.py
+++ plot_maps.py
@@ -1,3 +1,4 @@
+import sys
 from mpl_toolkits.basemap import Basemap, maskoceans
 import matplotlib
 matplotlib.use('Agg')
@@ -8,10 +9,14 @@ import numpy as np
 from numpy import linspace
 from numpy import meshgrid
 from collections import OrderedDict
+from scipy import interpolate
+from adjustText import adjust_text # Small package to improve label locations
 
-mmi_obs_file = 'data/1847HMMI.txt'
-infile = 'outputs/scenario_gmf_loc_mmi_1847_ChiouYoungs2008_gridded_clip.tif'
-bbox = '104/116/-10/-5'
+
+mmi_obs_file = sys.argv[1]# 'data/1847HMMI.txt'
+#infile = 'outputs/scenario_gmf_loc_mmi_1847_ChiouYoungs2008_gridded_clip.tif'
+infile = sys.argv[2] #r'outputs/scenario_gmf_loc_mmi_1847_ChiouYoungs2008().csv'
+bbox = sys.argv[3] #'104/116/-10/-5'
 
 # function fro roman numerals
 def write_roman(num):
@@ -74,7 +79,7 @@ m.drawparallels(np.arange(-90.,90.,2), labels=[1,0,0,0],
 m.drawmeridians(np.arange(0.,360.,2), labels=[0,0,0,1],
                 fontsize=10, dashes=[2, 2], color='0.5',
                 linewidth=0.5)
-
+"""
 ds = gdal.Open(infile)
 data = ds.GetRasterBand(1).ReadAsArray()
 #data = np.flipud(data)
@@ -102,12 +107,29 @@ xy_source = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
 xy=meshgrid(xy_source[0,:,0], xy_source[1][0])
 maskdata = maskoceans(xy[0], xy[1], data)
 data = np.ma.masked_where(maskdata==0, maskdata)
+"""
 clevs = np.arange(0.5,9.5,1.0)
 cmap = plt.get_cmap('jet')
 #norm = BoundaryNorm(clevs, ncolors=cmap.N, clip=True)
 #m.imshow(data, cmap=cmap, vmin=0, vmax=9, zorder=0, latlon=True)
-m.contourf(xy[0], xy[1], data, clevs, latlon=True)
-csm = m.contour(xy[0], xy[1], data, clevs, latlon=True, colors='k')
+
+data = np.genfromtxt(infile, delimiter=',')
+print data[:,0]
+minlon = np.min(data[:,0])
+maxlon = np.max(data[:,0])
+minlat = np.min(data[:,1])
+maxlat = np.max(data[:,1])
+xy = np.mgrid[minlon:maxlon:0.02,minlat:maxlat:0.02]
+xx,yy=meshgrid(xy[0,:,0], xy[1][0])
+griddata = interpolate.griddata((data[:,0], data[:,1]), data[:,2], (xx,yy), method='nearest')
+# Mask Oceans
+maskdata = maskoceans(xx, yy, griddata)
+m.drawlsmask(resolution='i', grid=2.5,ocean_color='w')
+m.contourf(xx, yy, maskdata, clevs, latlon=True)
+csm = m.contour(xx, yy, maskdata, clevs, latlon=True, colors='k')
+
+#m.contourf(xy[0], xy[1], data, clevs, latlon=True)
+#csm = m.contour(xy[0], xy[1], data, clevs, latlon=True, colors='k')
 #plt.clabel(csm, inline=1, fontsize=10, fmt='%0.2f')
 #m.pcolormesh(xy[0], xy[1], data, latlon=True)
 
@@ -117,12 +139,30 @@ for obs in mmi_obs[:,2]:
     mmi_labels.append(write_roman(int(obs)))
 m.scatter(mmi_obs[:,0], mmi_obs[:,1], c=mmi_obs[:,2], cmap=cmap, 
           vmin=0.5, vmax=8.5, latlon=True)
+texts = []
 for label, x, y in zip(mmi_labels, mmi_obs[:,0], mmi_obs[:,1]):
     x,y =  m(x,y)
-    plt.annotate(
-        label,
-        xy=(x, y), xytext=(-2, 2), xycoords='data', fontsize=11,
-        textcoords='offset points', ha='right', va='bottom')#,
+    texts.append(plt.text(x,y,label))
+#    texts.append(plt.annotate(label, xy=(x,y),
+#                          xycoords='data', fontsize=11))
+adjust_text(texts, only_move='xy',
+            arrowprops=dict(arrowstyle="->",
+                            color='k', lw=0.5))
+ax = plt.gca()
+for child in ax.get_children():
+#    print type(child)
+    if isinstance(child, matplotlib.text.Annotation):
+        print child.arrowprops
+#print dir(child)
+#        print child.arrow
+        print dir(child.arrow_patch)
+        print child.arrow_patch._posA_posB
+        print child.arrow_patch.get_path()
+        print child.arrow_patch.get_path_in_displaycoord()
+#    plt.annotate(
+#        label,
+#        xy=(x, y), xytext=(-2, 2), xycoords='data', fontsize=11,
+#        textcoords='offset points', ha='right', va='bottom')#,
 #        bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
 #        arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
 plt.gcf().subplots_adjust(bottom=0.1)
