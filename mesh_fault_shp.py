92a4ce86292d7d8b28e5f22c3f2d2a9fbbf78a4f
Jonathan Griffin <jono.d.griffin@gmail.com>
2018-01-11 21:10:56 +1100

    Code for writing fault data to shapefiles

---
+++ mesh_fault_shp.py
@@ -0,0 +1,86 @@
+"""Helper script to convert mesh points to a polygon 
+"""
+
+import numpy as np
+from write_fault_shp import fault2shp 
+infile = 'outputs/rupture_mesh_1818_YoungsEtAl1997SInter_Mw6p75_118p882_-8p102_13p62km.txt'
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def mesh2shp(infile):
+    """
+    """
+    output_shp = infile[:-3] + 'shp'
+
+    file_length = file_len(infile)
+    print file_length
+    
+    # Work out lines that have the required data
+    top_lons = 0
+    bottom_lons = int(file_length/3 - 1)
+    top_lats = int(file_length/3)
+    bottom_lats = int((file_length/3)*2 - 1)
+    top_depths = int((file_length/3)*2)
+    bottom_depths = int(file_length-1)
+    print top_lons,bottom_lons,top_lats,bottom_lats,top_depths,bottom_depths
+    
+    lons = []
+    lats = []
+    depths = []
+    with open(infile) as f:
+        for i, l in enumerate(f):
+            if i == top_lons:
+                lons.append([float(x) for x in l.split(',')])
+            if i == bottom_lons:
+                tmp_lons = ([float(x) for x in l.split(',')])
+                print tmp_lons
+                tmp_lons = list(reversed(tmp_lons))
+                lons.append(tmp_lons)
+
+            if i == top_lats:
+                lats.append([float(x) for x in l.split(',')])
+            if i == bottom_lats:
+                tmp_lats = ([float(x) for x in l.split(',')])
+                print tmp_lats
+                tmp_lats = list(reversed(tmp_lats))
+                lats.append(tmp_lats)
+
+            if i == top_depths:
+                depths.append([float(x) for x in l.split(',')])
+            if i == bottom_depths:
+                tmp_depths = ([float(x) for x in l.split(',')])
+                print tmp_depths
+                tmp_depths = list(reversed(tmp_depths))
+                depths.append(tmp_depths)
+    lons = np.array(lons).flatten()
+    lats = np.array(lats).flatten()
+    depths = np.array(depths).flatten()
+    print lons
+    print lats
+    print depths
+
+    fault2shp(lons, lats, output_shp, corner_depths = depths, vertice_array=True)
+
+if __name__ == "__main__":
+    infiles = [
+    'outputs/rupture_mesh_1699megathrust_AtkinsonBoore2003SInter_Mw8p05_107p292_-8p084_42p96km.txt',
+    'outputs/rupture_mesh_1699megathrust_YoungsEtAl1997SInter_Mw8p15_107p001_-7p880_39p11km.txt',
+    'outputs/rupture_mesh_1699megathrust_ZhaoEtAl2006SInter_Mw8p45_107p105_-8p337_32p28km.txt',
+    'outputs/rupture_mesh_1815_AtkinsonBoore2003SInter_Mw7p45_115p084_-7p824_9p08km.txt',
+    'outputs/rupture_mesh_1815_YoungsEtAl1997SInter_Mw7p45_114p634_-7p768_9p08km.txt',
+    'outputs/rupture_mesh_1815_ZhaoEtAl2006SInter_Mw7p45_114p904_-7p798_9p08km.txt',
+    'outputs/rupture_mesh_1818_AtkinsonBoore2003SInter_Mw6p95_118p925_-8p061_11p35km.txt',
+    'outputs/rupture_mesh_1818_YoungsEtAl1997SInter_Mw6p75_118p882_-8p102_13p62km.txt',
+    'outputs/rupture_mesh_1818_ZhaoEtAl2006SInter_Mw7p05_119p016_-8p058_11p35km.txt',
+    'outputs/rupture_mesh_1820_AtkinsonBoore2003SInter_Mw7p65_119p059_-8p017_9p08km.txt',
+    'outputs/rupture_mesh_1820_AtkinsonBoore2003SInter_Mw8p35_124p707_-7p920_9p08km.txt',
+    'outputs/rupture_mesh_1820_YoungsEtAl1997SInter_Mw7p85_118p288_-8p057_9p08km.txt',
+    'outputs/rupture_mesh_1820_YoungsEtAl1997SInter_Mw8p35_122p464_-8p251_9p08km.txt',
+    'outputs/rupture_mesh_1820_ZhaoEtAl2006SInter_Mw7p85_118p651_-8p036_9p08km.txt',
+    'outputs/rupture_mesh_1820_ZhaoEtAl2006SInter_Mw8p35_122p464_-8p251_9p08km.txt']
+    for infile in infiles:
+        mesh2shp(infile)
---
+++ write_fault_shp.py
@@ -0,0 +1,100 @@
+"""Write a fault or rupture geometry to shapefile
+"""
+
+import gdal, osr, ogr
+import os 
+from numpy import mean
+
+def fault2shp(corner_lons, corner_lats, output_shp, corner_depths=None, vertice_array=False):
+    """Function for writing a fault geometry to a shapefile
+    """
+    
+    # Create a Polygon from the extent tuple
+    ring = ogr.Geometry(ogr.wkbLinearRing)
+    #for i in range(len(corner_lons)):
+    # need to get in right order
+    if vertice_array:
+        print 'do something'
+        # Assume corner_lons, corner_lats are 2 1D array
+        # giving the corrdinates of the polygon boundary
+        for i in range(len(corner_lons)):
+            ring.AddPoint(corner_lons[i], corner_lats[i])
+        ring.AddPoint(corner_lons[0], corner_lats[0]) # close polygon
+    else:
+        ring.AddPoint(corner_lons[0],corner_lats[0])
+        ring.AddPoint(corner_lons[1],corner_lats[1])
+        ring.AddPoint(corner_lons[3],corner_lats[3])
+        ring.AddPoint(corner_lons[2],corner_lats[2])
+        ring.AddPoint(corner_lons[0],corner_lats[0]) # close polygon
+    poly = ogr.Geometry(ogr.wkbPolygon)
+    poly.AddGeometry(ring)
+
+    drv = ogr.GetDriverByName('ESRI Shapefile') 
+    # Remove output shapefile if it already exists
+    if os.path.exists(output_shp):
+        drv.DeleteDataSource(output_shp)
+    # Create the output shapefile
+    outDataSource = drv.CreateDataSource(output_shp)
+    outLayer = outDataSource.CreateLayer("Fault_geom", geom_type=ogr.wkbPolygon)
+    # Add an ID field
+    idField = ogr.FieldDefn("id", ogr.OFTInteger)
+    outLayer.CreateField(idField)
+    # Add a depth field
+    depthField = ogr.FieldDefn("mean_depth", ogr.OFTReal)
+    outLayer.CreateField(depthField)
+    # Create the feature and set values
+    featureDefn = outLayer.GetLayerDefn()
+    feature = ogr.Feature(featureDefn)
+    feature.SetGeometry(poly)
+    feature.SetField("id", 1)
+    feature.SetField("mean_depth", mean(corner_depths))
+    outLayer.CreateFeature(feature)
+    feature = None
+    # Save and close
+    outDataSource = None
+    drv = None
+
+    # Now write upper trace to line shapefile    
+    line = ogr.Geometry(ogr.wkbLineString)
+    corner_depths = list(corner_depths)
+    min_dep = min(corner_depths)
+    min_dep_index = corner_depths.index(min(corner_depths))
+    print min_dep_index
+    line.AddPoint(corner_lons[min_dep_index],corner_lats[min_dep_index])
+    corner_depths[min_dep_index] = 1e10
+    min_dep_2 = min(corner_depths)
+    min_dep_index_2 = corner_depths.index(min(corner_depths))
+    print min_dep_index_2
+    line.AddPoint(corner_lons[min_dep_index_2],corner_lats[min_dep_index_2])
+    corner_depths[min_dep_index] = min_dep
+    print min_dep, min_dep_2
+    mean_upper_depth = mean([min_dep, min_dep_2])
+    print mean_upper_depth
+    drv = ogr.GetDriverByName('ESRI Shapefile') 
+    output_shp = output_shp.rstrip('.shp') + '_upper_edge.shp'
+    # Remove output shapefile if it already exists
+    if os.path.exists(output_shp):
+        drv.DeleteDataSource(output_shp)
+    # Create the output shapefile
+    outDataSource = drv.CreateDataSource(output_shp)
+    outLayer = outDataSource.CreateLayer("Fault_geom", geom_type=ogr.wkbLineString)   
+    # Add a depth field
+    depthField = ogr.FieldDefn("mean_depth", ogr.OFTReal)
+    outLayer.CreateField(depthField)
+    # Create the feature and set values
+    featureDefn = outLayer.GetLayerDefn()
+    feature = ogr.Feature(featureDefn)
+    feature.SetGeometry(line)
+    feature.SetField("mean_depth", mean_upper_depth)
+    outLayer.CreateFeature(feature)
+    feature = None
+    # Save and close
+    outDataSource = None
+    drv = None
+
+if __name__=="__main__":
+    corner_lons =  [ 110.80301631, 110.14964724, 110.86646785, 110.21405031]
+    corner_lats = [-9.39397613, -9.31704483, -8.86453769, -8.78772005]
+    corner_depths = [ 32.81350292, 32.81350292, 74.01549338, 74.01549338]
+    fault2shp(corner_lons, corner_lats, 'test_fault.shp', corner_depths)
+    
