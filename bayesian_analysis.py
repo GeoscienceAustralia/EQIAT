85314bc26e8e624792e18d266e0c8632720e5968
Jonathan Griffin <jdg547@gadi-login-04.gadi.nci.org.au>
2023-08-13 20:49:20 +1000

    Fixed up location plotting

--- bayesian_analysis.py
+++ bayesian_analysis.py
@@ -8,7 +8,8 @@ July 2018
 
 import sys, os
 import numpy as np
-#from mpl_toolkits.basemap import Basemap, maskoceans
+import cartopy.crs as ccrs
+import cartopy.feature as cf
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -53,10 +54,10 @@ event_name = ''
 #gmpe_weights = [0.3, 0.1, 0.1, 0.5]
 #mmi_obs_file = 'data/1699HMMI_weighted_mod.txt'
 #num_params = 8 # Force estimation only of uncertainties
-data_files = ['outputs/1699megathrust_AtkinsonBoore2003SInter_parameter_llh.csv',
-              'outputs/1699megathrust_ZhaoEtAl2006SInter_parameter_llh.csv',
-              'outputs/1699megathrust_AbrahamsonEtAl2015SInter_parameter_llh.csv']
-gmpe_weights = [0.2, 0.3, 0.5]
+data_files = ['outputs/1699megathrust_AtkinsonBoore2003SInter_parameter_llh.csv']#,
+#              'outputs/1699megathrust_ZhaoEtAl2006SInter_parameter_llh.csv',
+#              'outputs/1699megathrust_AbrahamsonEtAl2015SInter_parameter_llh.csv']
+gmpe_weights = [1.0]#[0.2, 0.3, 0.5]
 mmi_obs_file = 'data/1699HMMI_weighted_mod.txt'
 num_params = 4 # reduce by 3 as strike, dip and depth dependent on location on plane
 #data_files = ['outputs/1840_CampbellBozorgnia2014_parameter_llh.csv',
@@ -189,8 +190,8 @@ num_params = 4 # reduce by 3 as strike, dip and depth dependent on location on p
 #event_name = '1852BandaDetachment' # deal with area source not using naming convention
 #event_name = '1852Banda_domain_ryan_mmi'
 mag_prior_type = 'GR' #'GR' #'uniform'
-mmi_obs_file = 'data/1852Banda_MMI_weight_rev.txt'
-num_params = 7
+#mmi_obs_file = 'data/1852Banda_MMI_weight_rev.txt'
+#num_params = 7
 set_sigma=True
 
 bbox_dict = {1699: '104/110/-10.5/-5',
@@ -536,9 +537,6 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
                     ax.scatter(x_addition, y_addition, marker = '*', c='b',
                        edgecolor='k', s=200, zorder=10)
                 
-#if key != 'latitude' and key != 'longitude':
-#            ax.plot(lbx, lby, color='k')
-#            ax.plot(ubx, uby, color='k')
             ax.set_ylim(0, ymax)
             ax.set_ylabel('Probability')
             ax.set_xlabel(xlabel_dict[key])
@@ -554,23 +552,6 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     pdf_sums = []
     all_lons = []
     all_lats = []
-##    for i, lon in enumerate(parameter_space[parameter_dict['longitude']]):
-#        # Get each unique longitude value
-##        if lon in all_lons:
-##            continue
-##        else:
-            # Get each unique latitude value and find intersection
-###            lat = parameter_space[parameter_dict['latitude']][i]
-            #               lat = self.parameter_pdf_values['latitude'][i]                                         
-##            for j, lat in enumerate(parameter_space[parameter_dict['latitude']]):
-##                index = np.intersect1d(np.argwhere(parameter_space[parameter_dict['longitude']]==lon), \
-##                                           np.argwhere(parameter_space[parameter_dict['latitude']]==lat))
-##                pdf_sum = np.sum(parameter_space[7][index])#uncert_fun.pdf(rmse[index]))
-##                pdf_sums.append(pdf_sum)
-##                all_lons.append(lon)
-##                all_lats.append(lat)
-    # Try a different way
-    # Need to get numpy 1.13 or above and use unique along axis=1
     lon_lat_pairs = []
     for ll,la in zip(parameter_space[parameter_dict['longitude']], 
                      parameter_space[parameter_dict['latitude']]):
@@ -586,7 +567,6 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     # Normalise pdf sums                                                                                                                   
         
     pdf_sums = np.array(pdf_sums)
-#    pdf_sums = pdf_sums/np.sum(pdf_sums)
     parameter_pdf_sums['lon_lat'] = pdf_sums
     all_lons = np.array(all_lons)
     all_lats = np.array(all_lats)
@@ -594,14 +574,15 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     data_dump = np.vstack((all_lons, all_lats, pdf_sums)).T
     np.savetxt('data_dump.csv', data_dump, delimiter=',', header='lon,lat,pdf_sum')
     # Get best fit value                                                                                                                   
-    
     index = np.argmin(parameter_space[6])
     best_fit_lon =  parameter_space[parameter_dict['longitude']][index]
     best_fit_lat =  parameter_space[parameter_dict['latitude']][index]
     index_posterior = np.argmax(parameter_space[7])
     best_fit_lon_posterior =  parameter_space[parameter_dict['longitude']][index_posterior]
     best_fit_lat_posterior =  parameter_space[parameter_dict['latitude']][index_posterior]
-    ax =  fig.add_subplot(gs[:,0:2])
+    proj=ccrs.PlateCarree()
+#    proj=ccrs.TransverseMercator()
+    ax =  fig.add_subplot(gs[:,0:2], projection=proj)
     bbox = bbox.split('/')
     if bbox is not None:
         minlon = float(bbox[0])
@@ -615,6 +596,13 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
         maxlat = max(parameter_pdf_values['latitude'])
     lat_0 = minlat + (maxlat-minlat)/2.
     lon_0 = minlon + (maxlon-minlon)/2.
+    # First cut with cartopy
+    ax.coastlines(linewidth=0.5,color='k')#, crs=proj)
+    ax.add_feature(cf.BORDERS, color='0.2')#, crs=proj)
+    print('minlon, maxlon, minlat, maxlat')
+    print(minlon, maxlon, minlat, maxlat)
+    ax.set_extent([minlon, maxlon, minlat, maxlat], crs=proj)
+    plt.savefig('test.png')
     """
     m = Basemap(projection='tmerc',
                 lat_0=lat_0, lon_0=lon_0,
@@ -626,6 +614,8 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     m.drawcoastlines(linewidth=0.5,color='k')
     m.drawcountries(color='0.2')
     m.drawstates(color='0.2')
+    """
+    
     if maxlon-minlon < 2:
         gridspace = 0.5
     elif maxlon-minlon < 3:
@@ -634,13 +624,11 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
         gridspace = 2.0
     else:
         gridspace = 5.0
-    m.drawparallels(np.arange(-90.,90.,gridspace), labels=[1,0,0,0],
-                    fontsize=10, dashes=[2, 2], color='0.5',
-                    linewidth=0.5)
-    m.drawmeridians(np.arange(0.,360.,gridspace), labels=[0,0,1,0],
-                    fontsize=10, dashes=[2, 2], color='0.5',
-                    linewidth=0.5)
-    """
+    # Add some gridlines    
+    gl = ax.gridlines(draw_labels=True, crs=proj)
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+
     max_val = max(pdf_sums)*1.1
 #    print 'pdf_sums', pdf_sums
     clevs = np.arange(0.0,max_val,(max_val/50))
@@ -651,27 +639,31 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     xy = np.mgrid[minlon:maxlon:res,minlat:maxlat:res]
     xx,yy=np.meshgrid(xy[0,:,0], xy[1][0])
     griddata = interpolate.griddata((all_lons, all_lats), pdf_sums, (xx,yy), method='nearest') # nearest # linear
-    """
-    # now plot filled contours of pdf                                                                                                              
-    cs = m.contourf(xx, yy, griddata, clevs, cmap=cmap, vmax=max_val, vmin=0.0, latlon=True)
-    for c in cs.collections: # Fix white space on contour levels for pdf images
-        c.set_edgecolor("face")
+    # now plot filled contours of pdf
+#    cs = ax.contourf(xx, yy, griddata, transform=proj) # minimal version for testing
+    cs = ax.contourf(xx, yy, griddata, clevs, cmap=cmap, vmax=max_val, vmin=0.0, transform=proj)#latlon=True)
+#    for c in cs.collections: # Fix white space on contour levels for pdf images
+#        c.set_edgecolor("face")
+    print('Contours plotted')
     # Mask areas outside of source model                                                                                                           
     if limits_filename is not None:
         limits_data = np.genfromtxt(limits_filename, delimiter=',')
         limits_x = limits_data[:,0]
         limits_y = limits_data[:,1]
-        limits_x, limits_y = m(limits_x, limits_y) # Convert to map coordinates                                                                    
+#        limits_x, limits_y = ax.transAxes.transform(limits_x, limits_y)
+##        limits_x, limits_y = m(limits_x, limits_y) # Convert to map coordinates                                                                    
         poly = Polygon(np.c_[limits_x, limits_y], closed=True)
         clippath =  poly.get_path()
-        ax = plt.gca()
+        #ax = plt.gca()
         patch = PathPatch(clippath, transform=ax.transData, facecolor='none', linewidth=0.4, linestyle='--')
         print('Adding patch')
         ax.add_patch(patch)
         for contour in cs.collections:
             contour.set_clip_path(patch)
-
-    # Now add some locations                                                                                                                               
+        print('Patch added')
+    #plt.tight_layout()
+    # Now add some locations
+    print('Add some locations here')
     if localities_file is not None:
         f_in = open(localities_file)
         loc_lon = []
@@ -682,49 +674,60 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
             loc_lon.append(float(row[0]))
             loc_lat.append(float(row[1]))
             loc_name.append(row[2])
-        loc_sp = m.scatter(loc_lon, loc_lat, c='k', s=40, marker='s', latlon=True)
+        print('loc_lon', loc_lon, type(loc_lon))
+        print('loc_lat', loc_lat)
+        loc_sp = ax.scatter(loc_lon, loc_lat, c='k', s=40, marker='s', transform=proj)#proj=projection)#latlon=True)
         texts = []
         for label, x, y in zip(loc_name, loc_lon, loc_lat):
-            x,y =  m(x,y)
-            texts.append(plt.text(x,y,label, fontsize=14, color='0.4', zorder=20))
+            #x,y =  m(x,y)
+            texts.append(plt.text(x,y,label, transform=proj, fontsize=14, color='0.4', zorder=20))
 #        adjust_text(texts, only_move='xy',
 #                    arrowprops=dict(arrowstyle="-",
 #                                    color='k', lw=0.5))
 
     # Now add historical points on top
+    print('Adding historical points to plot')
     if mmi_obs is not None:
         clevs = np.arange(0.5,9.5,1.0)
         cmap = plt.get_cmap('YlOrRd', 7)
         mmi_labels = []
         for obs in mmi_obs[:,2]:
             mmi_labels.append(write_roman(int(obs)))
-            sp = m.scatter(mmi_obs[:,0], mmi_obs[:,1], c=mmi_obs[:,2], cmap=cmap,
-                           vmin=1.5, vmax=8.5, s=30, latlon=True)
-            sp_ticks = np.arange(1,9,1)
+        print('mmi_obs[:,0]', mmi_obs[:,0], type(mmi_obs[:,0]))
+        print('mmi_obs[:,1]', mmi_obs[:,1])
+        sp = ax.scatter(mmi_obs[:,0], mmi_obs[:,1], c=mmi_obs[:,2], cmap=cmap,
+                        vmin=1.5, vmax=8.5, s=30, transform=ccrs.Geodetic())# latlon=True)
+        sp_ticks = np.arange(1,9,1)
         # Label only if there aren't too many to avoid plots being too busy
+        print('plotted')
         if len(mmi_obs[:,2]) < 20:
  #           texts = []
             for label, x, y in zip(mmi_labels, mmi_obs[:,0], mmi_obs[:,1]):
-                x,y =  m(x,y)
-                texts.append(plt.text(x,y,label, fontsize=10))
-        if len(texts) > 0:
-            adjust_text(texts, only_move='xy',
-                        arrowprops=dict(arrowstyle="-",
-                                        color='k', lw=0.5))
+#                x,y =  m(x,y)
+                texts.append(plt.text(x,y,label, fontsize=10, transform=proj))
+#        if len(texts) > 0:
+#            print('Adjusting texts')
+#            adjust_text(texts, only_move='xy',
+#                        arrowprops=dict(arrowstyle="-",
+#                                        color='k', lw=0.5))
         # Divide the axes to make the colorbar locatable to right of maps
         #divider = make_axes_locatable(ax)
         #cax = divider.append_axes("right", size="5%", pad=0.05)
        # plt.colorbar(im, cax=cax)
         #fig.add_axes(cax)
-        cbar1 = m.colorbar(sp, ticks=sp_ticks, location='right', pad = 0.2)
+#        cbar1 = m.colorbar(sp, ticks=sp_ticks, location='right', pad = 0.2)
+        print('set colour bar')
+        cbar1 = fig.colorbar(sp, ticks=sp_ticks, location='right', pad = 0.2)
         cbar1.ax.set_ylabel('MMI')
 
-    # Now add best-fit location on top                                                                     
-    m.scatter(best_fit_lon, best_fit_lat, marker = '*', facecolor='none', #c='#696969',
-              edgecolor='k', s=100, zorder=10, latlon=True)
-    m.scatter(best_fit_lon_posterior, best_fit_lat_posterior, marker = '*', facecolor='none',
-              edgecolor='k', s=500, zorder=9, latlon=True)
-    #m.text(0.05, 0.95, 'c)', transform=ax.transAxes, fontsize=14)                                                        
+    # Now add best-fit location on top
+    print('Adding best-fit location')
+    ax.scatter(best_fit_lon, best_fit_lat, marker = '*', facecolor='none', #c='#696969',
+              edgecolor='k', s=100, zorder=10, transform=proj)#latlon=True)
+    ax.scatter(best_fit_lon_posterior, best_fit_lat_posterior, marker = '*', facecolor='none',
+               edgecolor='k', s=500, zorder=9, transform=proj), #latlon=True)
+    #m.text(0.05, 0.95, 'c)', transform=ax.transAxes, fontsize=14)
+    print('Plot additions')
     if plot_additions is not None:
         try:
             x_addition = plot_additions['longitude']
@@ -732,8 +735,9 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
         except KeyError:
             x_addition = None
         if x_addition is not None:
-            m.scatter(x_addition, y_addition, marker = '*', c='b',
-                       edgecolor='k', s=200, zorder=10, latlon=True)
+            ax.scatter(x_addition, y_addition, marker = '*', c='b',
+                       edgecolor='k', s=200, zorder=10, transform=proj)#latlon=True)
+    print('Add annotation')
     plt.annotate('a)', xy=(-0.01, 1.01),xycoords='axes fraction', fontsize=14)
 #    print 'max_val', max_val
     if max_val < 0.0000001:
@@ -753,13 +757,15 @@ def parameter_pdf(parameter_space, fig_comment='', mmi_obs=None, limits_filename
     else:
         loc_int = 0.1
     ticks = np.arange(0.0, max_val*1.1, loc_int)
-    cbar = m.colorbar(cs, ticks=ticks, location='bottom')#orientation='horizontal')
+    print('Add another colour bar')
+    cbar = fig.colorbar(cs, ticks=ticks, location='bottom')#orientation='horizontal')
     cbar.ax.set_xlabel('Probability')
-    """
     figname = '%s_all_parameter_pdf.png' % (fig_comment)
     figname = figname.replace('()', '')
+    print('Call tight_layout')
     plt.tight_layout()
-    plt.savefig(figname, dpi=600, format='png', bbox_inches='tight')
+    print('Saving figure')
+    plt.savefig(figname, dpi=600, format='png', bbox_inches='tight', facecolor=fig.get_facecolor())
 #    plt.savefig(figname, dpi=600, format='pdf', bbox_inches='tight')
     
 
